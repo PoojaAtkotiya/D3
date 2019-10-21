4e39a823f4682982f54204ec57ffd7efa7ec0390
Pooja Atkotiya <35293527+PoojaAtkotiya@users.noreply.github.com>
2019-10-21 14:52:26 +0530

    update method done

--- js/Project1.js
+++ js/Project1.js
@@ -10,40 +10,86 @@ var svg = d3.select("#chart-area")
 var group = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var x = d3.scaleBand()
+    .range([0, width])
+    .paddingInner(0.2)
+    .paddingOuter(0.3);
+
+var y = d3.scaleLinear()
+    .range([height, 0]);
+
+var xAxisGroup = group.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(" + 0 + "," + height + ")")
+
+
+var yAxisGroup = group.append("g")
+    .attr("class", "y-axis");
+
+
+var yAxisCall = group.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", -(height / 2))
+    .attr("y", -60)
+    .attr("transform", "rotate(-90)")
+    .text("Revenue")
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle");
+
+group.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 70)
+    .text("Month")
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle");
+
 
 
 d3.json("data/revenues.json").then((data) => {
-    console.log(data);
-    var x = d3.scaleBand()
-        .domain(data.map((d) => { return d.month; }))
-        .range([0, width])
-        .paddingInner(0.2)
-        .paddingOuter(0.3);
+    //console.log(data);
 
-    var xAxisCall = d3.axisBottom(x)
-    group.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", "translate(" + 0 + "," + height + ")")
-        .call(xAxisCall)
-        .selectAll("text")
-        .attr("x", -5)
-        .attr("y", 10)
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)");
+    d3.interval(function () {
+        update(data);
+    }, 1000)
 
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => { return d.revenue; })])
-        .range([height, 0]);
+    update(data);
+
+});
+
+function update(data) {
+
+    x.domain(data.map((d) => { return d.month; }));
+    y.domain([0, d3.max(data, d => { return d.revenue; })]);
+
+    var xAxisCall = d3.axisBottom(x);
+    xAxisGroup.call(xAxisCall)
+    // .selectAll("text")
+    // .attr("x", -5)
+    // .attr("y", 10)
+    // .attr("text-anchor", "end")
+    // .attr("transform", "rotate(-40)");
 
     var yAxisCall = d3.axisLeft(y);
-    group.append("g")
-        .attr("class", "y-axis")
-        .call(yAxisCall)
+    yAxisGroup.call(yAxisCall);
+
+    //JOIN new data with old element
+    var rects = group.selectAll("rect")
+        .data(data);
+
+    //EXIT old elements not present in new data.
+    rects.exit().remove();
+
+    //UPDATE old elements present in new data
+    rects
+        .attr("x", d => { return x(d.month); })
+        .attr("y", d => { return y(d.revenue); })
+        .attr("width", x.bandwidth)
+        .attr("height", d => { return height - y(d.revenue); });
 
 
-    group.selectAll("rect")
-        .data(data)
-        .enter()
+    //ENTER new elements present in new data.
+    rects.enter()
         .append("rect")
         .attr("x", d => {
             //console.log(x(d.month));
@@ -56,32 +102,4 @@ d3.json("data/revenues.json").then((data) => {
         .attr("width", x.bandwidth)
         .attr("height", d => { return height - y(d.revenue); })
         .attr("fill", "grey");
-
-    //var line = group.append("line").attr("x1", 0).attr("y1", 0).attr("x2", 50).attr("y2", 50).attr("stroke", "black");
-
-    var lineX = d3.scaleTime().range([0, width]);
-    var lineY = d3.scaleLinear().range([height, 0]);
-    
-    var line = d3.line()
-        .x(d => { return x(d.month) })
-        .y(d => { return y(d.revenue) });
-
-       
-    group.selectAll("line")
-        .data(data)
-        .enter()
-        .append("line")
-        .attr("x1", d => {
-            //console.log(x(d.month));
-            //return x(d.month);
-            return 0;
-        })
-        .attr("y1", d => {
-            // console.log(y(d.revenue));
-            // return y(d.revenue);
-            return 0;
-        })
-        .attr("x2", 50).attr("y2", 50)
-        .attr("stroke", "black");
-
-});
+}
